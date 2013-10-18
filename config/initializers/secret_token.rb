1109cfc28121960673abfbaf1fd7decdbf2982eb
Anuvind Venu Menon <manuvind@gmail.com>
2013-10-18 13:22:47 -0700

    Initialized rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VolunteerScheduler::Application.config.secret_token = '696f19e1b3fc0aa3617485ed74c45dd7b9878879198f46592b3d970c57cbf0039940f0fea44755542efea30c50e52bf72241300abf064b94ae5c5ab985e18fe0'
