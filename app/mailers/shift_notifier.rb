d22105e154b67786bffc2e8a3c2fd4c2ea1306d0
Anuvind Venu Menon <manuvind@gmail.com>
2013-11-24 15:14:34 -0800

    Add default heroku email to shift notify.

--- app/mailers/shift_notifier.rb
+++ app/mailers/shift_notifier.rb
@@ -1,9 +1,9 @@
 class ShiftNotifier < ActionMailer::Base
-  default from: "volunteer-scheduler@heroku.com"
+  default from: "app19355191@heroku.com"
 
   def shift_notify(volunteer, shift)
   	@volunteer = volunteer
   	@shift = shift
-  	mail(:to => "#{@volunteer.name} <manuvind@gmail.com>", :subject => "Reminder for #{@shift.title}")
+  	mail(:to => "#{@volunteer.name} <#{@volunteer.email}>", :subject => "Reminder for #{@shift.title}")
   end
 end
