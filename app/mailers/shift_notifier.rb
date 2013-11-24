f0392f92de0056f8ae5c1dff445a498cf3939389
Anuvind Venu Menon <manuvind@gmail.com>
2013-11-24 00:31:07 -0800

    Add delayed_job gem to schedule email jobs, remove sendgrid tag from landing page, change notifier method.

--- app/mailers/shift_notifier.rb
+++ app/mailers/shift_notifier.rb
@@ -4,6 +4,6 @@ class ShiftNotifier < ActionMailer::Base
   def shift_notify(volunteer, shift)
   	@volunteer = volunteer
   	@shift = shift
-  	mail(:to => "#{volunteer.name} <#{volunteer.email}>", :subject => "Reminder for #{shift.title}")
+  	mail(:to => "#{@volunteer.name} <#{@volunteer.email}>", :subject => "Reminder for #{@shift.title}")
   end
 end
