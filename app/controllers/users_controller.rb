2fb2cbb7f39fb7d8309f19b5a2e61825ad876b0d
saasbook <saasbook@saasbook.(none)>
2013-12-01 18:58:05 -0800

    Admin users able to add other users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
+  before_filter :is_admin
+
+  def is_admin
+    if !current_user.is_admin?
+      redirect_to events_path
+    end
+  end
+
   def new
     @user = User.new
   end
@@ -8,7 +16,7 @@ class UsersController < ApplicationController
 
     if @user.save
       flash[:notice] = "Successfully created User." 
-      redirect_to root_path
+      redirect_to user_index_path
     else
       render :action => 'new'
     end
@@ -21,9 +29,12 @@ class UsersController < ApplicationController
   end
 
   def destroy
+    User.find(params[:id]).destroy
+    redirect_to user_index_path
   end
 
   def index
+    @users = User.all
   end
 
   def show
