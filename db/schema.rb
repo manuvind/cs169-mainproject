7f15afa911706387d6c56cdda8548d6378d129c0
Anuvind Venu Menon <manuvind@gmail.com>
2013-12-07 21:26:58 -0800

    Remove delayed_job from schema.

--- db/schema.rb
+++ db/schema.rb
@@ -13,22 +13,6 @@
 
 ActiveRecord::Schema.define(:version => 20131208025919) do
 
-  create_table "delayed_jobs", :force => true do |t|
-    t.integer  "priority",   :default => 0, :null => false
-    t.integer  "attempts",   :default => 0, :null => false
-    t.text     "handler",                   :null => false
-    t.text     "last_error"
-    t.datetime "run_at"
-    t.datetime "locked_at"
-    t.datetime "failed_at"
-    t.string   "locked_by"
-    t.string   "queue"
-    t.datetime "created_at",                :null => false
-    t.datetime "updated_at",                :null => false
-  end
-
-  add_index "delayed_jobs", ["priority", "run_at"], :name => "delayed_jobs_priority"
-
   create_table "events", :force => true do |t|
     t.string   "title"
     t.text     "description"
@@ -77,8 +61,9 @@ ActiveRecord::Schema.define(:version => 20131208025919) do
     t.string   "name"
     t.string   "email"
     t.string   "phone"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",                   :null => false
+    t.datetime "updated_at",                   :null => false
+    t.boolean  "recurring",  :default => true
     t.boolean  "temp"
   end
 
